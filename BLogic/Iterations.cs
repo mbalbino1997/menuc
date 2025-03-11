29663964e3cf98b39a8a296e035ef675b4914915
Massimo <a.delorenzo@betacom.it>
2025-03-11 08:31:22 +0100

    Aggiungere i file di progetto.

---
+++ BLogic/Iterations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartAcademy7.BLogic
+{
+    public class Iterations
+    {
+    public static void whileIteration()
+        {
+            bool isOk=true;
+            string input = "";
+            while (isOk)
+            {
+                Console.WriteLine("Sei nel while");
+           string param = Console.ReadLine();
+            isOk =param=="fine" ? false : true ;
+
+            }
+
+        }
+    public static void doWhileIteration()
+        {
+            bool isOk = true;
+            do
+            {
+                Console.WriteLine("Sei nel do while");
+                string param = Console.ReadLine();
+                isOk = param == "fine" ? false : true;
+            } while (isOk);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using StartAcademy7.BLogic;
+
+namespace StartAcademy7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool whileTrue = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Benvenuto nel menù");
+                Console.WriteLine("a per le iterazioni x per uscire");
+                string menuChoice = Console.ReadLine().ToLower();
+                switch (menuChoice)
+                {
+                    case "a":
+                        Iterations.doWhileIteration();
+                        Iterations.whileIteration();
+                        whileTrue = true;
+                        break;
+                    case "x":
+                        whileTrue = false;
+                        break;
+                    default: Console.WriteLine("Comando non valido");
+                        whileTrue = true;
+                        break;
+
+                }
+
+
+
+            } while (whileTrue);
+        }
+    }
+}
